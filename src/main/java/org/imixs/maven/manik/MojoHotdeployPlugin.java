3b28e3bf239f727f8582cc4b36dff8ebd49763c2
Ralph Soika <ralph.soika@imixs.com>
2022-08-31 23:10:40 +0200

    started version 2.0
    Issue #11

--- src/main/java/org/imixs/maven/manik/MojoHotdeployPlugin.java
+++ src/main/java/org/imixs/maven/manik/MojoHotdeployPlugin.java
@@ -1,13 +1,21 @@
 package org.imixs.maven.manik;
 
-import java.io.File;
+import java.io.IOException;
 import java.nio.file.ClosedWatchServiceException;
 import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -18,11 +26,37 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
+/**
+ * The Imixs Manik Hotdeploy Plugin can be started by the goal 'hotdeploy' This
+ * goal can be used to hotdeploy changed made in a source directory.
+ * <p>
+ * Plugin configuration example:
+ * 
+ * <pre>
+ * {@code		
+	<hotdeployments>
+		<deployment>
+			<source>src/main/webapp</source>
+			<target>docker/deployments/imixs-admin.war</target>
+		</deployment>
+	</hotdeployments>
+  }
+ * </pre>
+ * 
+ * This plugin was inspired by
+ * https://github.com/fizzed/maven-plugins/blob/master/watcher/src/main/java/com/fizzed/maven/watcher/RunMojo.java
+ * 
+ * See also:
+ * https://docs.oracle.com/javase/tutorial/essential/io/notification.html
+ * 
+ * @author rsoika
+ *
+ */
 @Mojo(name = "hotdeploy", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME, threadSafe = true)
 public class MojoHotdeployPlugin extends AbstractMojo {
-	
+
 	public static final String SEPARATOR = FileSystems.getDefault().getSeparator();
-	
+
 	/*
 	 * Note, the property variable name must be equal here!
 	 */
@@ -32,80 +66,123 @@ public class MojoHotdeployPlugin extends AbstractMojo {
 	@Parameter(defaultValue = "${project}", readonly = true)
 	private MavenProject project;
 
-	private WatchService watchService;
+	private WatchService watcher;
 
+	private Map<WatchKey, Path> watchKeyMap;
+
+	@SuppressWarnings("unchecked")
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		
+
 		int count = 0;
+		this.watchKeyMap = new HashMap<>();
 
 		String baseDir = project.getBasedir().toString();
 
 		try {
-			watchService = FileSystems.getDefault().newWatchService();
+			watcher = FileSystems.getDefault().newWatchService();
 		} catch (Exception e) {
 			throw new MojoExecutionException("Unable to create watch service");
 		}
-		getLog().info("Registering " + hotdeployments.size() + " sources...");
 
-		for (HotDeployment deployment : hotdeployments) {
-			String source = deployment.getSource();
-			String target = deployment.getTarget();
-			// relative path?
-			if (!source.startsWith(SEPARATOR)) {
-				source = baseDir + SEPARATOR + source;
-			}
-			if (!target.startsWith(SEPARATOR)) {
-				target = baseDir + SEPARATOR + target;
+		if (hotdeployments.size() > 0) {
+			getLog().info("Starting hot deployment....");
 
-				// verify if the target is a directory
-				File file = new File(target);
-				if (!file.isDirectory()) {
-					// skip....
-					getLog().warn("..... " + target
-							+ " is not a directory! Deployment will be skipped. Please check your plugin configuration.");
-					continue;
+			for (HotDeployment deployment : hotdeployments) {
+				String source = deployment.getSource();
+				String target = deployment.getTarget();
+
+				// relative path?
+				if (!source.startsWith(SEPARATOR)) {
+					source = baseDir + SEPARATOR + source;
+				}
+				if (!target.startsWith(SEPARATOR)) {
+					target = baseDir + SEPARATOR + target;
 				}
 				// test if target folder ends with /
 				if (!target.endsWith(SEPARATOR)) {
 					target = target + SEPARATOR;
 				}
+				getLog().info("..... source: " + source);
+				getLog().info("..... target: " + target);
+
+				Path sourcePath = Paths.get(source);
+				Path targetPath = Paths.get(target);
+				try {
+					// register watchKey for all event types
+					
+					registerRecursive(sourcePath,targetPath);
+					
+//					WatchKey watchKey = sourcePath.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
+//							StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+//					watchKeyMap.put(watchKey, targetPath);
+//					
+					getLog().info("..... watchKey registered");
+				} catch (IOException x) {
+					System.err.println(x);
+				}
 
 			}
-			
-			getLog().info("..... source=" + deployment.getSource());
-			getLog().info("..... target=" + deployment.getTarget());
 		}
 
-		long longTimeout = 60 * 60 * 24 * 1000L;
-		long shortTimeout = 750L;
-		long timeout = longTimeout;
-		int dueToRunGoal = 0;
-
-		while (true) {
+		System.out.println("===============> ..starting watcher loop....");
+		for (;;) {
 			try {
 
-				if (timeout > shortTimeout) {
-					getLog().info("Watcher - waiting for changes...");
+				// wait for key to be signaled
+				WatchKey key;
+				try {
+					key = watcher.take();
+				} catch (InterruptedException x) {
+					return;
 				}
 
-				// timeout to poll for (this way we can let lots of quick changes
-				// take place -- and only run the goal when things settles down)
-				WatchKey watchKey = watchService.poll(timeout, TimeUnit.MILLISECONDS);
-				if (watchKey == null) {
-					System.out.println("===============> ..tu irgendwas");
-				}
-				// schedule the goal to run
-				timeout = shortTimeout;
-				dueToRunGoal++;
+				// get the targetPath from the watchKeyMap....
+				Path targetPath = watchKeyMap.get(key);
+				// process all events...
+				for (WatchEvent<?> event : key.pollEvents()) {
+					WatchEvent.Kind<?> kind = event.kind();
+					// an OVERFLOW event can occur regardless if events are lost or discarded.
+					if (kind == StandardWatchEventKinds.OVERFLOW) {
+						continue;
+					}
 
-				watchKey.reset();
-			} catch (InterruptedException e) {
-				break;
+					
+					// The filename is the
+					// context of the event.
+					WatchEvent<Path> ev = (WatchEvent<Path>) event;
+					Path filename = ev.context();
+
+					System.out.println(" event: " + " kind=" + event.kind() + " file=" +  filename.toString());
+					System.out.println(" targetPath= " + targetPath.toString());
+
+					// do something....
+				}
+				// Reset the key -- this step is critical if you want to
+				// receive further watch events. If the key is no longer valid,
+				// the directory is inaccessible so exit the loop.
+				boolean valid = key.reset();
+				if (!valid) {
+					break;
+				}
 
 			} catch (ClosedWatchServiceException e) {
 				break;
 			}
 		}
 	}
-	// ...
+	
+	
+	
+	
+	private void registerRecursive(final Path root, Path targetPath) throws IOException {
+	    // register all subfolders
+	    Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
+	        @Override
+	        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+	           WatchKey key = dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+	           watchKeyMap.put(key, targetPath);
+	            return FileVisitResult.CONTINUE;
+	        }
+	    });
+	}
 }
